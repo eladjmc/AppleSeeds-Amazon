0ebc69f6ca2cc0cda0351a9c59282fc00e7ed854
eladjmc <eladjmc88@gmail.com>
2023-01-15 16:53:29 +0200

    added carusela scroling with left and right buttons

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,42 @@
 const smallImages = document.querySelectorAll(".small-image");
 const bigImage = document.querySelector(".big-image");
+const karuselaCurrentPage = document.querySelector(".current-page");
+const karuselaToggleLeft = document.querySelector(".left-carousel-button");
+const karuselaToggleRight = document.querySelector(".right-carousel-button");
+const karuselaPageItems1 = document.querySelectorAll(".karosela-page1");
+const karuselaPageItems2 = document.querySelectorAll(".karosela-page2");
+let KaruselaPage = 1;
+let totalPrice = document.querySelector(".total-price-amount");
+
+karuselaToggleLeft.addEventListener('click', () => {
+    if(KaruselaPage != 1){
+        karuselaPageItems1.forEach(card => {
+            card.style = `display : block;`;
+        });
+
+        karuselaPageItems2.forEach(card => {
+            card.style = `display : none;`;
+        });
+         KaruselaPage = 1;
+         karuselaCurrentPage.innerHTML='Page 1'
+    }
+});
+karuselaToggleRight.addEventListener('click', () => {
+    if(KaruselaPage != 2){
+        karuselaPageItems2.forEach(card => {
+            card.style = `display : block;`;
+        });
+         
+        karuselaPageItems1.forEach(card => {
+            card.style = `display : none;`;
+        });
+        KaruselaPage = 2;
+        karuselaCurrentPage.innerHTML='Page 2'
+    }
+});
+
+
+
 
 const onEnter = (event,index) => {
     bigImage.style =`background: url("/assets/images/display_item${index}.jpg") no-repeat center center/cover;`
